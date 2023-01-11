794a8c5726dc27d88c87d46a5c4bb642e1e5b834
Domenico <domenicoderasmo99@gmail.com>
2023-01-11 16:48:55 -0500

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    let letters = string.split("");
+    for (let i = letters.length - 1; i >= 0; i--) {
+        result += letters[i];
+    }
+    return result;
 };
 
 // Do not edit below this line

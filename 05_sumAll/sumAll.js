9f0623e76bc77f96c2e6b12d05d0f64aa2a049cf
Domenico <domenicoderasmo99@gmail.com>
2023-01-11 17:02:48 -0500

    Exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, last) {
+    if (typeof(start) !== "number" || typeof(last) !== "number" || start < 0 || last < 0) {return "ERROR";}
+    let result = 0;
+    if (start > last) {
+        for (let i = last; i <= start; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = start; i <= last; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line

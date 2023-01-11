c6dce8fb67a8b1010e0da2d3c92b607624b3ee03
Domenico <domenicoderasmo99@gmail.com>
2023-01-11 16:58:41 -0500

    Exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        let index = 0;
+        let found = false;
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === arguments[i]) {
+                found = true;
+                index = j;
+            }
+        }
+        if (found) {array.splice(index, 1);}
+    }
+    return array;
 };
 
 // Do not edit below this line
